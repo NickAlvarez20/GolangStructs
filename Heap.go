b2955e1ebdde717662e82b0e353ce8954eec68f5
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2024-12-20 18:19:52 -0700

    Added Nested Structs(Family) | Added Oldest Member method | Instantiated a few people | Created an instance of family struct | Added printing method with formatting to main func

--- Heap.go
+++ Heap.go
@@ -25,12 +25,38 @@ func (p Person) IsAdult() bool{
 	return isAdult
 }
 
+//Nested Structs-Create a Family struct with a slice of Person. Implement a method to find the oldest family member
+type Family struct{
+	Members []Person
+}
+
+func (f Family) OldestMember() Person {
+	var oldest Person
+	for _, member := range f.Members{
+		if member.age > oldest.age{
+			oldest = member
+		}
+	}
+	return oldest
+}
+
 //Create main func to run executable
 //Create an instance of Person struct
 func main(){
+	//Instantiate a few people
 p1 := Person{name: "Elon Tusk", age: 53}
+p2 := Person{name: "Grok Musk", age: 2500}
+p3 := Person{name: "Dusk Musk", age: 22}
+//Print information about one person
 fmt.Printf("%s is %d years old.\n",p1.name,p1.age)
-p1.IsAdult()
+fmt.Printf("%s is an adult: %v\n",p1.name, p1.IsAdult())
+//Create an instance of Family with members
+familyGood := Family{
+	Members: []Person{p1,p2,p3},
+}
+//Find and print the oldest family member
+oldest := familyGood.OldestMember()
+fmt.Printf("The oldest person in the family is %s, aged %d\n",oldest.name,oldest.age)
 }
 
 //Generics
